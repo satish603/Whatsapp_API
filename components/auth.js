9da256fb796940c71b6a8803d5ddd0c9edcf2a79
satish chauhan <satishchauhan603@gmail.com>
2022-06-25 00:39:55 +0530

    api updated

--- components/auth.js
+++ components/auth.js
@@ -1,47 +1,71 @@
-const router = require('express').Router();
-const fs = require('fs');
+const router = require("express").Router();
+const fs = require("fs");
 
-router.get('/checkauth', async (req, res) => {
-    client.getState().then((data) => {
-        console.log(data)
-        res.send(data)
-    }).catch((err) => {
-        if (err) {
-            res.send("DISCONNECTED")
-            try {
-                fs.unlinkSync('../session.json')
-            } catch(err) {
-                console.log(err)
-            }
-        }
+router.get("/checkauth", async (req, res) => {
+  client
+    .getState()
+    .then((data) => {
+      console.log(data);
+      res.send(data);
     })
+    .catch((err) => {
+      if (err) {
+        res.send("DISCONNECTED");
+      }
+    });
 });
 
-router.get('/getqr', (req,res) => {
-    var qrjs = fs.readFileSync('components/qrcode.js');
+router.get("/getqr", async (req, res) => {
+  client
+    .getState()
+    .then((data) => {
+      if (data) {
+        res.write("<html><body><h2>Already Authenticated</h2></body></html>");
+        res.end();
+      } else sendQr(res);
+    })
+    .catch(() => sendQr(res));
+});
 
-    fs.readFile('components/last.qr', (err,last_qr) => {
-        fs.readFile('session.json', (serr, sessiondata) => {
-            if (err && sessiondata) {
-                res.write("<html><body><h2>Already Authenticated</h2></body></html>");
-                res.end();
-            } else if (!err && serr) {
-                var page = `
+function sendQr(res) {
+  fs.unlink('components/last.qr', function(err) {
+    if(err && err.code == 'ENOENT') {
+        // file doens't exist
+        console.info("File doesn't exist, won't remove it.");
+    } else if (err) {
+        // other errors, e.g. maybe we don't have enough permission
+        console.error("Error occurred while trying to remove file");
+    } else {
+        console.info(`removed`);
+    }
+});
+  fs.readFile("components/last.qr", (err, last_qr) => {
+    if (!err && last_qr) {
+      var page = `
                     <html>
                         <body>
-                            <script>${qrjs}</script>
+                            <script type="module">
+                            </script>
                             <div id="qrcode"></div>
-                            <script type="text/javascript">
-                                new QRCode(document.getElementById("qrcode"), "${last_qr}");
+                            <script type="module">
+                                import QrCreator from "https://cdn.jsdelivr.net/npm/qr-creator/dist/qr-creator.es6.min.js";
+                                let container = document.getElementById("qrcode");
+                                QrCreator.render({
+                                    text: "${last_qr}",
+                                    radius: 0.5, // 0.0 to 0.5
+                                    ecLevel: "H", // L, M, Q, H
+                                    fill: "#536DFE", // foreground color
+                                    background: null, // color or null for transparent
+                                    size: 256, // in pixels
+                                }, container);
                             </script>
                         </body>
                     </html>
-                `
-                res.write(page)
-                res.end();
-            }
-        })
-    });
-});
+                `;
+      res.write(page);
+      res.end();
+    }
+  });
+}
 
 module.exports = router;
