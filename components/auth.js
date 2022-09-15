bf3860b8d9c2d9af36658c6c78455d233e9113b1
satish chauhan <satishchauhan603@gmail.com>
2022-09-15 19:26:45 +0530

    Status Down TO UP

--- components/auth.js
+++ components/auth.js
@@ -28,17 +28,6 @@ router.get("/getqr", async (req, res) => {
 });
 
 function sendQr(res) {
-  fs.unlink('components/last.qr', function(err) {
-    if(err && err.code == 'ENOENT') {
-        // file doens't exist
-        console.info("File doesn't exist, won't remove it.");
-    } else if (err) {
-        // other errors, e.g. maybe we don't have enough permission
-        console.error("Error occurred while trying to remove file");
-    } else {
-        console.info(`removed`);
-    }
-});
   fs.readFile("components/last.qr", (err, last_qr) => {
     if (!err && last_qr) {
       var page = `
--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const fs = require('fs');
-const axios = require('axios');
-const shelljs = require('shelljs');
-const cors = require("cors");
+const fs = require("fs");
+const axios = require("axios");
+const shelljs = require("shelljs");
+const puppeteer = require('puppeteer');
 
-const config = require('./config.json');
-const { Client } = require('whatsapp-web.js');
-const SESSION_FILE_PATH = process.env.SESSION_FILE_PATH || './session.json';
+const config = require("./config.json");
+const { Client, LocalAuth } = require("whatsapp-web.js");
 
-let sessionCfg;
-if (fs.existsSync(SESSION_FILE_PATH)) {
-    sessionCfg = require(SESSION_FILE_PATH);
-}
 
 process.title = "whatsapp-node-api";
+puppeteer.launch({ args: ['--no-sandbox'] });
 global.client = new Client({
-    puppeteer: {
-        headless: true,
-        args: [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--unhandled-rejections=strict'
-    ]},
-    session: sessionCfg
+  authStrategy: new LocalAuth(),
+  puppeteer: {
+		args: ['--no-sandbox'],
+	}
 });
 
 global.authed = false;
@@ -32,96 +24,74 @@ const app = express();
 
 const port = process.env.PORT || config.port;
 //Set Request Size Limit 50 MB
-app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-
-client.on('qr', qr => {
-    fs.writeFileSync('./components/last.qr', qr);
+client.on("qr", (qr) => {
+  console.log("new qr gen");
+  fs.writeFileSync("./components/last.qr", qr);
 });
 
+client.on("authenticated", () => {
+  console.log("AUTH!");
+  authed = true;
 
-client.on('authenticated', (session) => {
-    console.log("AUTH!");
-    sessionCfg = session;
-
-    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
-        if (err) {
-            console.error(err);
-        }
-        authed = true;
-    });
-
-    try {
-        fs.unlinkSync('./components/last.qr')
-    } catch(err) {}
+  try {
+    fs.unlinkSync("./components/last.qr");
+  } catch (err) {}
 });
 
-client.on('auth_failure', () => {
-    console.log("AUTH Failed !")
-    sessionCfg = ""
-    process.exit()
+client.on("auth_failure", () => {
+  console.log("AUTH Failed !");
+  process.exit();
 });
 
-client.on('ready', () => {
-    console.log('Client is ready!');
-    
+client.on("ready", () => {
+  console.log("Client is ready!");
 });
-client.on('message', async msg => {
-    console.log('MESSAGE RECEIVED', msg);
 
-    if (msg.body === 'hello') {
-        // Send a new message as a reply to the current one
-        msg.reply('hello sir/madam how can i help you.');
-    } });
-    // client.on('message', message => {
-    //     if(message.body === '!ping') {
-    //         client.sendMessage(message.from, 'pong');
-    //     }
-    // });
-
-// client.on('message', msg => {
-//     if (config.webhook.enabled) {
-//         axios.post(config.webhook.path, { msg })
-//     }
-// })
+client.on("message", async (msg) => {
+  if (config.webhook.enabled) {
+    if (msg.hasMedia) {
+      const attachmentData = await msg.downloadMedia();
+      msg.attachmentData = attachmentData;
+    }
+    axios.post(config.webhook.path, { msg });
+  }
+});
+client.on("disconnected", () => {
+  console.log("disconnected");
+});
 client.initialize();
 
-const chatRoute = require('./components/chatting');
-const groupRoute = require('./components/group');
-const authRoute = require('./components/auth');
-const contactRoute = require('./components/contact');
+const chatRoute = require("./components/chatting");
+const groupRoute = require("./components/group");
+const authRoute = require("./components/auth");
+const contactRoute = require("./components/contact");
 
-
-app.use(function(req, res, next){
-    console.log(req.method + ' : ' + req.path);
-    next();
+app.use(function (req, res, next) {
+  console.log(req.method + " : " + req.path);
+  next();
 });
-app.use('/chat',chatRoute);
-app.use('/group',groupRoute);
-app.use('/auth',authRoute);
-app.use('/contact',contactRoute);
-
-app.listen(port, () => {
-    console.log("Server Running Live on Port : " + port);
-});
+app.use("/chat", chatRoute);
+app.use("/group", groupRoute);
+app.use("/auth", authRoute);
+app.use("/contact", contactRoute);
 
 app.get("/", (req, res, next) => {
-    return res.json({ message: "whatsapp API Server Running" });
-  });
+  return res.json({ message: "whatsapp API Server Running" });
+});
 
-  app.get("/testapi", (req, res, next) => {
-    axios.post('http://localhost:5000/chat/sendmessage/914354344554', {// ph number wih your country code.91 here indicates for india
-    message: 'hello world from heroku docker',})
-    .then(function (response) {
-      res.send("Message sent to the user")
-      console.log(response);
-    })
-    .catch(function (error) {
-      res.send("Error Message not sent")
-      console.log(error);
-    });
-    });
+app.get("/qr", (req, res, next) => {
+  if (authed) {
+    return res.sendFile("./components/last.qr");
+  } else {
+    return res.send("Not Authenticated");
+  }
+});
+
+app.listen(port, () => {
+  console.log("Server Running Live on Port : " + port);
+});
